534c80d2cecbfb78de592eeb3c2d10813f868b25
ketan-😎 <ketan.chaudhari@in.ncs-i.com>
2024-04-10 10:23:54 +0530

    tailwind-dark mode and switch button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,32 @@
 <script setup lang="ts">
-import InputComponent from './components/InputComponent.vue'
+import InputComp from './components/InputComp.vue'
 import RenderChart from './components/RenderChart.vue'
+import SelectComp from './components/SelectComp.vue'
+import HeaderComp from './components/HeaderComp.vue'
+import { provide, ref, watch } from 'vue'
+
+const isLightMode = ref(true)
+
+provide('isLightMode', isLightMode);
+
+watch(isLightMode, (newMode) => {
+  console.log(newMode)
+})
 </script>
 
 <template>
-  <InputComponent />
-  <select>
-    <option>daily</option>
-    <option>hourly</option>
-  </select>
-  <RenderChart />
+  <div class="flex w-full flex-col">
+    <div class="w-full z-[9999] top-0 left-0 fixed h-14 flex">
+      <HeaderComp />
+    </div>
+    <div class="pt-14 w-full min-h-screen">
+      <h1 class="text-3xl">Welcome to {{ isLightMode }}</h1>
+      <SelectComp />
+      <div class="flex">
+        <InputComp />
+      </div>
+      <RenderChart />
+    </div>
+  </div>
 </template>
 <style scoped></style>
---
+++ frontend/src/components/HeaderComp.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { inject, ref, watch, type Ref } from 'vue'
+import { Switch } from '@headlessui/vue'
+import Sun from '@/assets/icons/sun.svg'
+import Moon from '@/assets/icons/moon.svg'
+
+
+const isLightMode: Ref<boolean> = inject('isLightMode', ref(true))
+
+watch(isLightMode, (lightsPlz) => {
+  if (lightsPlz) {
+    document.documentElement.classList.remove('dark')
+  } else {
+    document.documentElement.classList.add('dark')
+  }
+})
+</script>
+
+<template>
+  <div class="bg-white flex w-full shadow align-middle">
+    <div class="flex justify-between w-full items-center p-3">
+      <div class="cursor-pointer text-red-600 dark:text-green-500">Hello 1</div>
+      <div class="flex gap-5">
+        <div class="py-16">
+          <Switch
+            v-model="isLightMode"
+            :class="isLightMode ? 'bg-teal-900' : 'bg-teal-700'"
+            class="relative inline-flex h-[38px] w-[74px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+          >
+            <span class="sr-only">Use setting</span>
+            <img
+              aria-hidden="true"
+              :src="isLightMode ? Sun: Moon"
+              :class="isLightMode ? 'translate-x-9' : 'translate-x-0'"
+              class="pointer-events-none p-[5px] inline-block h-[34px] w-[34px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out"
+            />
+          </Switch>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
